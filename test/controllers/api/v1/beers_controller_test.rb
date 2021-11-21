bc3e452687d4e4f3b4d26698eda9b8628d753c86
Mike de Groot - Remote <mike@entwurf.co.uk>
2021-11-21 15:45:52 +0000

    test for create successfully written

--- test/controllers/api/v1/beers_controller_test.rb
+++ test/controllers/api/v1/beers_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
+# this integration test tests our api endpoint response
 class Api::V1::BeersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "index request returns json" do
+    # beer = beers(:one)
+    get "/api/v1/beers/index", xhr: true
+  
+    # returns a 200 res
+    assert_response :success
+    # it sends json back
+    assert_equal "application/json; charset=utf-8", @response.content_type
+  end
+
+  test "POST create creates a resource" do
+    post( "/api/v1/beers/create", params: { 'brand' => "corona" }, xhr: true)
+
+    # returns a 200 res
+    assert_response :success
+    # it sends json back
+    assert_equal "application/json; charset=utf-8", @response.content_type
+    body = JSON.parse(response.body)
+    # returned object matches the passed in name
+    assert_equal "corona", body["brand"]
+  end
 end
--- test/models/beer_test.rb
+++ test/models/beer_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class BeerTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
 
   test "should not save beer without brand" do
     beer = Beer.new
+    # uncomment this line to add a title and see if the record saves
+    # beer.brand = 'nonono'
     assert_not beer.save , "saved the beer without a brand" # this message shows in the test output if the test fails
+  end
+
+  test "should destroy beer" do
+    beer = Beer.new
+    assert beer.destroy
   end
 
 end
